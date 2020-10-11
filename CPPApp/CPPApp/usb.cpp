6b071f5854829c3304ff2d9d2cc9539a3ab46d8f
Nelson Amador <rod6214@gmail.com>
2020-10-11 16:41:51 -0300

    Created CPPApp and included winusb generic driver

---
+++ CPPApp/CPPApp/usb.cpp
@@ -0,0 +1,11 @@
+#include "usb.h"
+
+
+bool USB::Find_Device(unsigned short p_VendorID,
+						unsigned short p_PoductID,
+						void* pReadHandle,
+						void* pWriteHandle) 
+{
+
+	return false;
+}
---
+++ CPPApp/CPPApp/usb.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef CUSTOM_USB_H
+#define CUSTOM_USB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+	class USB 
+	{
+		public:
+			static bool Find_Device(unsigned short p_VendorID,
+									unsigned short p_PoductID,
+									void* pReadHandle, 
+									void* pWriteHandle);
+	};
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif // !CUSTOM_USB_H
